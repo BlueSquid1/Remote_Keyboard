3424d6c3448000932e2730a90f478ad90c97053d
Clinton Page <clinton.page123@gmail.com>
2016-11-12 10:51:51 +1030

    deleted windows 8 and uwp

---
+++ Remote_Keyboard/Remote_Keyboard/UDPListerner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+
+
+namespace Remote_Keyboard
+{
+    class UDPListerner
+    {
+        
+        //async means run concurrently
+        public async void StartingListening()
+        {
+
+        }
+    }
+}
---
+++ Remote_Keyboard/Remote_Keyboard/UdpListener.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+
+
+namespace Remote_Keyboard
+{
+    class UdpListener
+    {
+        private UdpClient udpListener;
+
+        //constructor
+        public UdpListener( int portNum )
+        {
+            this.udpListener = new UdpClient(portNum);
+        }
+
+        //async means run concurrently
+        public async void StartingListening()
+        {
+            while (true)
+            {
+                var result = await this.udpListener.ReceiveAsync();
+                var message = Encoding.ASCII.GetString(result.Buffer);
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
